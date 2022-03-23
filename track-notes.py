0c4ac87066e4400e9826da71c0b5f9232cc91ba1
Jesse Millar <hellojessemillar@gmail.com>
2022-03-23 14:50:55 -0500

    Add files via upload

--- track-notes.py
+++ track-notes.py
@@ -5,37 +5,89 @@
 #pip install pypiwin32
 
 import irsdk
+import time
 import pyttsx3
 import json
 
-converter = pyttsx3.init()
-voices = converter.getProperty('voices')
-converter.setProperty('voice', voices[1].id)
+# this is our State class, with some helpful variables
+class State:
+    ir_connected = False
+    lastAnnouncedPercent = 200 # Set to something higher than 100 as the "null" value
+    notes = []
 
-ir = irsdk.IRSDK()
-ir.startup()
+# here we check if we are connected to iracing
+# so we can retrieve some data
+def check_iracing():
+    if state.ir_connected and not (ir.is_initialized and ir.is_connected):
+        state.ir_connected = False
+        # don't forget to reset your State variables
+        state.lastAnnouncedPercent = 200
+        # we are shutting down ir library (clearing all internal variables)
+        ir.shutdown()
+        print('irsdk disconnected', flush=True)
+    elif not state.ir_connected and ir.startup() and ir.is_initialized and ir.is_connected:
+        state.ir_connected = True
+        print('irsdk connected', flush=True)
 
-lastAnnouncedPercent = 200 # Set to something higher than 100 as the "null" value
+# our main loop, where we retrieve data
+# and do something useful with it
+def loop():
+    # on each tick we freeze buffer with live telemetry
+    # it is optional, but useful if you use vars like CarIdxXXX
+    # this way you will have consistent data from those vars inside one tick
+    # because sometimes while you retrieve one CarIdxXXX variable
+    # another one in next line of code could change
+    # to the next iracing internal tick_count
+    # and you will get incosistent data
+    ir.freeze_var_buffer_latest()
 
-if ir['WeekendInfo']:
-    # TODO Live reload file
-    trackName = ir['WeekendInfo']['TrackDisplayName'] + " " + ir['WeekendInfo']['TrackConfigName']
-    f = open(trackName+'.json')
-    allNotes = json.load(f)
-    f.close()
+    try:
+        if ir['WeekendInfo']:
+            trackName = ir['WeekendInfo']['TrackDisplayName'] + " " + ir['WeekendInfo']['TrackConfigName']
+            f = open(trackName+'.json')
+            jsonFile = json.load(f)
+            if jsonFile is not None:
+                state.notes = jsonFile
+            f.close()
+    except ValueError:  # includes simplejson.decoder.JSONDecodeError
+        print("Check JSON", flush=True)
+        
+    if ir['LapDistPct']:
+        percent = int(ir['LapDistPct'] * 100)
+        #print(percent, flush=True)
 
-    while True:
-        if ir['LapDistPct']:
-            percent = int(round(ir['LapDistPct'], 2) * 100)
-            #print(percent)
+        for note in state.notes['notes']:
+            if note['percent'] == percent:
+                converter.say(note['note'])
+                converter.runAndWait()
 
-            for note in allNotes['notes']:
-                if note['percent'] == percent:
-                    converter.say(note['note'])
-                    converter.runAndWait()
+        if state.notes['debug'] == True:
+            if percent % 10 == 0 and percent != state.lastAnnouncedPercent:
+                state.lastAnnouncedPercent = percent
+                converter.say(str(percent) + "percent")
+                converter.runAndWait()
 
-            if allNotes['debug'] == True:
-                if percent % 10 == 0 and percent != lastAnnouncedPercent:
-                    lastAnnouncedPercent = percent
-                    converter.say(str(percent) + "percent")
-                    converter.runAndWait()
+if __name__ == '__main__':
+    # initializing ir and state
+    ir = irsdk.IRSDK()
+    state = State()
+
+    converter = pyttsx3.init()
+    voices = converter.getProperty('voices')
+    converter.setProperty('voice', voices[1].id)
+
+    try:
+        # infinite loop
+        while True:
+            # check if we are connected to iracing
+            check_iracing()
+            # if we are, then process data
+            if state.ir_connected:
+                loop()
+            # sleep for 1 second
+            # maximum you can use is 1/60
+            # cause iracing updates data with 60 fps
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # press ctrl+c to exit
+        pass
